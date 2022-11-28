0ac1bd7afd270ce84193d12cc066b961bab614da
Austin Clark <asclark109@gmail.com>
2022-11-27 23:20:32 -0600

    add auctions, cam-service routing

--- src/main/java/com/example/gateway/GatewayApplication.java
+++ src/main/java/com/example/gateway/GatewayApplication.java
@@ -12,6 +12,8 @@ import org.springframework.context.annotation.Bean;
 public class GatewayApplication {
     String userService = "http://user-service:8080";
     String watchlistService = "http://watchlist:8080";
+    String auctionsService = "http://auctions-service:10000";
+    String camService = "http://cam-service:51224";
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
@@ -30,6 +32,16 @@ public class GatewayApplication {
                 .route(p -> p
                         .path("/watchlist")
                         .uri(watchlistService))
+
+                // AUCTIONS
+                .route(p -> p
+                        .path("/api/v1/Auctions/**","/api/v1/Bids/**","/api/v1/ItemsUserHasBidsOn/**","/api/v1/cancelAuction/**","/api/v1/stopAuction/**","/api/v1/activeAuctions/**")
+                        .uri(auctionsService))
+
+                // AUCTIONS
+                .route(p -> p
+                        .path("/api/v1/closedauctions/**")
+                        .uri(camService))
 
 
                 .build();
